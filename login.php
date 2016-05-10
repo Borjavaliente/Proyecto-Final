c42dee2fe05bada0773af7e48b796eb3f553433a
Borjavaliente <fbvaliente22@gmail.com>
2016-05-10 23:22:18 +0200

    actualizacion dia 10
    Ya logea y el panel del admin tiene q modificarse

--- login.php
+++ login.php
@@ -3,31 +3,75 @@
 	include('conexion.php');
 	$username= $_POST["username"];
 	$passwd=$_POST["pass"];
+	$contrasenacod=md5($passwd);
 	//session_start();
 	//include('apiconexion.php');
 	//include('apimd5.php');
 	#Si se han fijado los 2 post se conectara con la base de datos
-	if($_POST){
-		//$passwd1=encriptar($passwd,1);
-		#se guarda en una variable la consultaza de mysql
-		$consultanick= mysql_query("SELECT nick FROM usuario WHERE nick='$username'") or die(mysql_error());
-		$consultapass=mysql_query("SELECT pass FROM usuario WHERE pass='$passwd'") or die(mysql_error());
-		$consultarol =mysql_query("SELECT rol FROM usuario") or die(mysql_error());
-		if($consultapass==$passwd && $consultanick==$nick && $consultarol=='admin'){
-			#Se redirige a la web del admin
-			header('Location:webadmin.php');
-			$_SESSION['rol']=$consultarol;
-			$_SESSION['nick']=$username;
-		}
-		if($consultapass==$passwd && $consultanick==$nick && $consultarol=='usuario'){
-			#Se redirige a la web del usuario
-			header('Location:webuser.php');
-			$_SESSION['rol']=$consultarol;
-			$_SESSION['nick']=$username;
-		}
-		//else{
-		//	header('Location:index.php');
-		//}
-		//desconectar();
+	if ($_POST) {
+		# code...
+		$query =mysql_query("SELECT * FROM usuario WHERE nick='$username' and pass='$passwd'");
+		$query2 = mysql_query("SELECT * FROM usuario WHERE nick='$username' and pass='$contrasenacod'");
+		$numrows = mysql_num_rows($query);
+		$numrows2 = mysql_num_rows($query2);
 	}
+	if ($numrows!=0){
+	  //bucle while que pilla todas las lineas de la consulta y las almacena como $row.
+	    while ($row = mysql_fetch_assoc($query)){
+	      session_start();
+	      $_SESSION['estadousuario']= $row['estado'];
+	      $comprobarbaneo=$_SESSION['estadousuario'];
+	      $_SESSION['rolusuario'] = $row['rol'];
+	      $compruebarol = $_SESSION['rolusuario'];
+	      #if ($comprobarbaneo =='bloqueado') {
+	        # header('./img/baneado.html');
+	      #}
+	      if ($compruebarol == 'admin') {
+	        $_SESSION['rolusuario'] = 'admin';
+	        header("location: webadmin.php");
+	      }
+	      if ($compruebarol == 'usuario') {
+	        $_SESSION['rolusuario'] = 'usuario';
+	        header("location: webuser.php");
+	      }
+	      //(se inicia la sesion para almacenar variables de sesion).
+	      //A partir de este punto, esta claro que quien ha hecho login es un usuario
+	      //valido de la tabla.
+	      //Se almacenan todas las variables de sesion, almacenados en las filas de la consulta.
+	      //para obtener una fila es con la sintaxis:
+	      //$row['nombredelafiladelatabla']
+	      //($row porque es la variable que almacena las filas, que se definen en el while).
+	      //-----------------------------------------------------------------------------------
+	    }
+	  }
+	  if ($numrows2!=0) {
+	    while ($row = mysql_fetch_assoc($query2)){
+	      session_start();
+	      $_SESSION['estadousuario']= $row['estado'];
+	      $comprobarbaneo=$_SESSION['estadousuario'];
+	      $_SESSION['rolusuario'] = $row['rol'];
+	      $compruebarol = $_SESSION['rolusuario'];
+	      #if ($comprobarbaneo =='bloqueado') {
+	        #redirect('baneado.html');
+	      #}
+	      }
+	      if ($compruebarol == 'admin') {
+	        header("location: webadmin.php");
+	      }
+	      if ($compruebarol != 'admin') {
+	        header("location: webuser.php");
+	      }
+	      //-----------------------------------------------------------------------------------
+	  }
+
+
+
+
+
+
+
+
+
+
+
  ?>

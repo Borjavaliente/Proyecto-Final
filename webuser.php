2079205cafc3a14598e9c79be128fedf56dfef12
Borjavaliente <fbvaliente22@gmail.com>
2016-05-11 19:49:53 +0200

    actualizacion dia 11 de mayo
    Ya validamos y registramos usuarios. Falta comprobar la sesion

--- webuser.php
+++ webuser.php
@@ -3,10 +3,49 @@
     <head>
         <meta charset="utf-8">
         <title></title>
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="webuser.css" media="screen" title="no title" charset="utf-8">
     </head>
     <body>
-        <p>
-            hola munso
-        </p>
+        <div class="container">
+	        <div class="row">
+		        <div class="col-lg-3 col-sm-6">
+                    <div class="card hovercard">
+                        <div class="cardheader"></div>
+                    <div class="avatar">
+                        <img alt="" src="http://rfidbrasil.com/blog/wp-content/uploads/2013/06/Fernando-Pessoa00.jpg">
+                    </div>
+                    <div class="info">
+                            <div class="title">
+                                <a target="_blank" href="http://scripteden.com/">Script Eden</a>
+                            </div>
+                        <div class="desc">Passionate designer</div>
+                        <div class="desc">Curious developer</div>
+                        <div class="desc">Tech geek</div>
+                    </div>
+                    <div class="bottom">
+                        <a class="btn btn-primary btn-twitter btn-sm" href="https://twitter.com/webmaniac">
+                            <i class="fa fa-twitter"></i>
+                        </a>
+                        <a class="btn btn-danger btn-sm" rel="publisher"
+                           href="https://plus.google.com/+ahmshahnuralam">
+                            <i class="fa fa-google-plus"></i>
+                        </a>
+                        <a class="btn btn-primary btn-sm" rel="publisher"
+                           href="https://plus.google.com/shahnuralam">
+                            <i class="fa fa-facebook"></i>
+                        </a>
+                        <a class="btn btn-warning btn-sm" rel="publisher" href="https://plus.google.com/shahnuralam">
+                            <i class="fa fa-behance"></i>
+                        </a>
+                    </div>
+                  </div>
+                </div>
+        	</div>
+        </div>
+        <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+        <!-- usar siempre la última versión de jquery de los servidores de google. Yo recomiendo esta -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
     </body>
 </html>

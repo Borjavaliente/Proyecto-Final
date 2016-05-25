838ae1d41eff6ab1af6ad7b372afac94bfd7f1b4
Borjavaliente <fbvaliente22@gmail.com>
2016-05-25 22:42:22 +0200

    Acerca de en prototipo
    Sin funcionalidad... solo diseño

---
+++ webuser_acerca.php
@@ -0,0 +1,335 @@
+<?php  ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="webuser.css" media="screen" title="no title" charset="utf-8">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css" media="screen" title="no title" charset="utf-8">
+    </head>
+    <body>
+        <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a target="_blank" href="#" class="navbar-brand">Paga y Dale</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="">Noticias</a></li>
+                         <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Explora
+                            <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Ayudanos!!</a></li>
+                                <li><a href="#">Acerca de...</a></li>
+                                <li><a href="#">Contacto</a></li>
+                            </ul>
+                         </li>
+                     </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <span class="glyphicon glyphicon-user"></span> 
+                                <strong>Nombre</strong>
+                                <span class="glyphicon glyphicon-chevron-down"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <div class="navbar-login">
+                                        <div class="row">
+                                            <div class="col-lg-4">
+                                                <p class="text-center">
+                                                    <span class="glyphicon glyphicon-user icon-size"></span>
+                                                </p>
+                                            </div>
+                                            <div class="col-lg-8">
+                                                <p class="text-left"><strong>Nombre Apellido</strong></p>
+                                                <p class="text-left small">correoElectronico@email.com</p>
+                                                <p class="text-left">
+                                                    <a href="#" class="btn btn-primary btn-block btn-sm">Actualizar Datos</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="divider"></li>
+                                <li>
+                                    <div class="navbar-login navbar-login-session">
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <p>
+                                                    <a href="#" class="btn btn-danger btn-block">Cerrar Sesion</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul>
+                        <form action="" class="search-form">
+                                 <div class="form-group has-feedback">
+                                   <label for="search" class="sr-only">Explora</label>
+                                   <input type="text" class="form-control" name="search" id="search" placeholder="search">
+                                       <span class="glyphicon glyphicon-search form-control-feedback"></span>
+                               </div>
+                         </form>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <br><br><br><br>
+        <div class="container">
+          <div class="row">
+            <div class="span12">
+              <div class="head">
+                <div class="row-fluid">
+                    <div class="span12">
+                        <div class="span6">
+                            <h1 class="muted" style="text-align:center">¿Por qué trabajar con nosotros?</h1>
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br><br><br>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                    <div class="icon-left">
+                      <i class="fa fa-thumbs-o-up fa-3x"></i>
+                    </div>
+                    <div class="text">
+                        <h4> Porque el Crowfounding es el futuro </h4>
+                       <p>Todos lo saben</p>
+                    </div> <br>
+                    <div class="icon-left">
+                      <i class="fa fa-codepen fa-3x"></i>
+                    </div>
+                    <div class="text">
+                    <h4> Porque puedes ganar dinero cumpliendo tus sueños </h4>
+                       <p>pasta</p>
+                    </div> <br>
+                    <div class="icon-left">
+                      <i class="fa fa-ioxhost fa-3x"></i>
+                    </div>
+                    <div class="text">
+                    <h4> Porque conoceras gente con tus mismas inquietudes </h4>
+                       <p>Conoce gente que te solucione las dudas</p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                    <div class="icon-left">
+                        <i class="fa fa-money fa-3x"></i>
+                    </div>
+                    <div class="text">
+                    <h4> Porque no necesitas mucho dinero para disfrutar </h4>
+                       <p>Ningun juego puede superar los 5 €</p>
+                    </div><br>
+                     <div class="icon-left">
+                        <i class="fa fa-taxi fa-3x"></i>
+                    </div>
+                    <div class="text">
+                    <h4> Porque el unico medio es Internet </h4>
+                       <p>Olvidate de oficinas, atascos, multitudes, hora punta...</p>
+                    </div><br>
+                     <div class="icon-left">
+                        <i class="fa fa-leaf fa-3x"></i>
+                    </div>
+                    <div class="text">
+                    <h4> Porque ayudar a los demas a crecer es nuestra prioridad </h4>
+                       <p>¿Quien puede mejorar un negocio que ayuda a la gente a crecer como desarrollador con una
+                           comisión tan infima?  SOLO EL 3%</p>
+                    </div>
+                </div>
+        	</div>
+        </div>
+        <br><br><br><br>
+        <div class="container">
+          <div class="row">
+            <div class="span12">
+              <div class="head">
+                <div class="row-fluid">
+                    <div class="span12">
+                        <div class="span6">
+                            <h1 class="muted" style="text-align:center">Plataformas utilizadas</h1>
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br><br><br>
+        <div class="container">
+        	<div class="row">
+        		<!-- Carousel -->
+            	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        			<!-- Wrapper for slides -->
+        			<div class="carousel-inner">
+        			    <div class="item active">
+        			    	<img src="img/clickteam.jpg" alt="First slide">
+                            <!-- Static Header -->
+                            <div class="header-text hidden-xs">
+                                <div class="col-md-12 text-center">
+                                    <h2>
+                                    	<span><strong>Clickteam Fusion</strong></span>
+                                    </h2>
+                                    <h3>
+                                    	<span>Clickteam es un software de empresa fundada en 1993 por François Lionet , Yves Lamoureux y Francis Poulain y con sede en Paris, Francia . Clickteam es quizas mejor conocido por la creación de una secuencia de comandos exento herramienta de programación que permite a los usuarios crear juegos de video u otro software interactivo utilizando una gama de GUI herramientas.<br>
+                                        Es muy recomendable para iniciados en el mundillo del desarrollo de videojuegos.</span>
+                                    </h3>
+                                </div>
+                            </div><!-- /header-text -->
+        			    </div>
+        			    <div class="item">
+        			    	<img src="img/Game.jpg" alt="Second slide">
+        			    	<!-- Static Header -->
+                            <div class="header-text hidden-xs">
+                                <div class="col-md-12 text-center">
+                                    <h2>
+                                        <span><strong>Game Make Studio</strong></span>
+                                    </h2>
+                                    <br>
+                                    <h3>
+                                    	<span>GameMaker es una herramienta de desarrollo rápido de aplicaciones, basada en un lenguaje de programación interpretado y un kit de desarrollo de software (SDK) para desarrollar videojuegos, creado por el profesor Mark Overmars en el lenguaje de programación Delphi, y orientado a usuarios novatos o con pocas nociones de programación. Overmars liberó la primera versión pública el 15 de noviembre de 1999.
+                                        <br>Tuvo su origen en los años 1990, cuando Mark Overmars empezó a crear una herramienta de animación para ayudar a sus estudiantes. Con el tiempo su proyecto se convirtió en una herramienta de desarrollo de videojuegos.</span>
+                                    </h3>
+                                </div>
+                            </div><!-- /header-text -->
+        			    </div>
+        			    <div class="item">
+        			    	<img src="img/love2d.jpg" alt="Third slide">
+        			    	<!-- Static Header -->
+                            <div class="header-text hidden-xs">
+                                <div class="col-md-12 text-center">
+                                    <h2>
+                                        <span>Love 2D-Game</span>
+                                    </h2>
+                                    <br>
+                                    <h3>
+                                    	<span>¡Hola! El amor es un marco impresionante que puede utilizar para hacer que los juegos en 2D en Lua. <br>Es gratis, de código abierto y funciona en Windows, Mac OS X, Linux, Android y iOS y aunque no es muy conocido goza de una amplia fama en EEUU y Japon.</span>
+                                    </h3>
+                                </div>
+                            </div><!-- /header-text -->
+        			    </div>
+                        <div class="item">
+        			    	<img src="img/phaser.jpg" alt="Third slide">
+        			    	<!-- Static Header -->
+                            <div class="header-text hidden-xs">
+                                <div class="col-md-12 text-center">
+                                    <h2>
+                                        <span>Phaser</span>
+                                    </h2>
+                                    <br>
+                                    <h3>
+                                    	<span>Phaser es un marco de JavaScript para el desarrollo de juegos en plataformas de escritorio y móviles. Si el dispositivo es capaz entonces se utiliza para la prestación de WebGL, pero por lo demás se revierte a la lona.<br> Es simplemente JavaScript, y no utiliza ninguna práctica de imitación de estilo OO internamente. No hay ningún sistema masivo cadena de herencia o componente, y usted no tiene que forzar a los objetos en ninguna estructura de clases fijo, tampoco. Es un simple cadena de prototipo sencillo, la forma JavaScript estaba destinado a ser utilizado.</span>
+                                    </h3>
+                                </div>
+                            </div><!-- /header-text -->
+        			    </div>
+        			</div>
+        			<!-- Controls -->
+        			<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+        		    	<span class="glyphicon glyphicon-chevron-left"></span>
+        			</a>
+        			<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+        		    	<span class="glyphicon glyphicon-chevron-right"></span>
+        			</a>
+        		</div><!-- /carousel -->
+        	</div>
+        </div>
+        <br><br><br>
+        <!-- botones sociales -->
+        <div class="container">
+            <div class="row">
+                <hr>
+                <div class="text-center">
+                <h4>Social Network</h4>
+                    <!-- just add href= for your links, like this: -->
+                    <a href="http://facebook.com" class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>
+                    <a class="btn btn-social-icon btn-dropbox"><i class="fa fa-dropbox"></i></a>
+                    <a class="btn btn-social-icon btn-github"><i class="fa fa-github"></i></a>
+                    <a class="btn btn-social-icon btn-google-plus"><i class="fa fa-google-plus"></i></a>
+                    <a class="btn btn-social-icon btn-linkedin"><i class="fa fa-linkedin"></i></a>
+                    <a class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></a>
+                 </div>
+                 <br>
+            </div> <!-- /.row -->
+        </div>
+        <br><br>
+        <!--  footer -->
+        <footer>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-4 col-sm-6 footerleft ">
+                <div class="logofooter"> Logo</div>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
+                <p><i class="fa fa-map-pin"></i> 210, Aggarwal Tower, Rohini sec 9, New Delhi -        110085, INDIA</p>
+                <p><i class="fa fa-phone"></i> Phone (India) : +91 9999 878 398</p>
+                <p><i class="fa fa-envelope"></i> E-mail : info@webenlance.com</p>
+              </div>
+              <div class="col-md-2 col-sm-6 paddingtop-bottom">
+                <h6 class="heading7">GENERAL LINKS</h6>
+                <ul class="footer-ul">
+                  <li><a href="#"> Career</a></li>
+                  <li><a href="#"> Privacy Policy</a></li>
+                  <li><a href="#"> Terms & Conditions</a></li>
+                  <li><a href="#"> Client Gateway</a></li>
+                  <li><a href="#"> Ranking</a></li>
+                  <li><a href="#"> Case Studies</a></li>
+                  <li><a href="#"> Frequently Ask Questions</a></li>
+                </ul>
+              </div>
+              <div class="col-md-3 col-sm-6 paddingtop-bottom">
+                <h6 class="heading7">LATEST POST</h6>
+                <div class="post">
+                  <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2015</span></p>
+                  <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2015</span></p>
+                  <p>facebook crack the movie advertisment code:what it means for you <span>August 3,2015</span></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+        <!--footer start from here-->
+        <div class="copyright">
+          <div class="container">
+            <div class="col-md-6">
+              <p>© 2016 - All Rights with Webenlance</p>
+            </div>
+            <div class="col-md-6">
+              <ul class="bottom_ul">
+                <li><a href="#">webenlance.com</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact us</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <!-- usar siempre la última versión de jquery de los servidores de google. Yo recomiendo esta -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script type="text/javascript" src="Recursillos/bootstrap/js/bootstrap.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/grid.js"></script>
+    <script type="text/javascript" src="bootstrap/js/jquery.fullpage.js"></script>
+    <script type="text/javascript" src="bootstrap/js/main.js"></script>
+    <script type="text/javascript" src="jquery.min.js"></script>
+    </body>
+</html>

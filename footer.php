20e22e510143ae7b92033f1eaf20a7fa5efa09b7
Borjavaliente <fbvaliente22@gmail.com>
2016-06-13 04:08:26 +0200

    footer con links
    actualizado a prueba de error

--- footer.php
+++ footer.php
@@ -21,7 +21,7 @@
         </ul>
       </div>
       <div class="col-md-3 col-sm-6 paddingtop-bottom">
-        <h6 class="heading7">LATEST POST</h6>
+        <h6 class="heading7">Ultimos Post</h6>
         <div class="post">
           <p>Microsoft vs Steam <span>August 3,2015</span></p>
           <p>Aple cambia su logo por una manzana podrida<span>August 3,2015</span></p>
@@ -29,7 +29,7 @@
         </div>
       </div>
       <div class="col-md-1 col-sm-2 footerright ">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1599.0660267046414!2d-4.422552691737619!3d36.71938964497947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72f795b1d07239%3A0x65ed7397264abe67!2sCalle+Marqu%C3%A9s+de+Larios%2C+M%C3%A1laga!5e0!3m2!1ses!2ses!4v1465153693214" width="400" height="250" frameborder="0" style="border:0" allowfullscreen style="padding-top:20px"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1599.0660267046414!2d-4.422552691737619!3d36.71938964497947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72f795b1d07239%3A0x65ed7397264abe67!2sCalle+Marqu%C3%A9s+de+Larios%2C+M%C3%A1laga!5e0!3m2!1ses!2ses!4v1465153693214" width="450" height="350" frameborder="0" style="border:0" allowfullscreen style="padding-top:20px"></iframe>
       </div>
     </div>
   </div>
@@ -39,14 +39,13 @@
 <div class="copyright">
   <div class="container">
     <div class="col-md-6">
-      <p>© 2016 - All Rights with Webenlance</p>
+      <p>© 2016 - Pagina Web hecha con bootstrap</p>
     </div>
     <div class="col-md-6">
       <ul class="bottom_ul">
-        <li><a href="#">webenlance.com</a></li>
-        <li><a href="#">About us</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Contact us</a></li>
+        <li><a href="">About us</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">Contact us</a></li>
       </ul>
     </div>
   </div>
